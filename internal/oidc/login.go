59e663fa846db585f12563e749d934367321540e
Nikolay Stupak <nstupak@i-core.ru>
2019-09-26 18:11:14 +0300

    oidc: fix typos

--- internal/oidc/login.go
+++ internal/oidc/login.go
@@ -25,7 +25,7 @@ type LoginConfig struct {
 	Endpoint      string // an OpenID Connect endpoint
 	ClientID      string // a client's ID
 	RedirectURI   string // a client's redirect uri
-	Scopes        string // an OpenID Connec scopes
+	Scopes        string // OpenID Connec scopes
 	Username      string // a user's name
 	Password      string // a user's password
 	PasswordStdin bool   // a user's password from stdin
